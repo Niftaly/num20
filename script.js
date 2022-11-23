388d0217cb47e96e59f8cc2bc58f8828867cb567
Niftaly <108418135+Niftaly@users.noreply.github.com>
2022-11-23 17:56:31 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+// let cvb = prompt('Reqem daxil edin')
+// let x = 1;
+
+// if(cvb == x || cvb == x+2) {
+//     alert('Tek reqem daxil etdiniz')
+// }else if(cvb =2) {
+//     alert('Cut eded daxil etdiniz')
+// }else {
+//     alert('Cut eded daxil etdiniz')
+// };
+
+
+// let tek = '';
+// let cut = '';
+
+// function Check() {
+
+//     if(a) {
+
+//     }
+// }
+
+
+function Check() {
+    
+    let clicked = document.querySelector('#inp').value;
+
+    let data = "";
+    for(let i = 0; i<clicked; i++) {
+        
+    }
+    
+}
+
+
